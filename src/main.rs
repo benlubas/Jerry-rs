cca790866e1863ab43db6ff9b0ba224a3eb38509
benlubas <benmlubas@gmail.com>
2022-07-27 19:19:59 -0400

    adding rps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use rand::Rng;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
@@ -17,16 +18,38 @@ impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         let prefix: String = msg.content.chars().take(2).collect();
         if prefix != ">>" {
-            return
+            return;
         }
         let message: String = msg.content.chars().skip(2).collect();
         match message.as_str().trim() {
-            "ping" => if let Err(why) = msg.channel_id.say(&ctx.http, "pong!").await {
-                println!("Error sending message: {:?}", why);
-            }, 
-            "remind" => if let Err(why) = msg.channel_id.say(&ctx.http, "@goomfy buy a controller").await {
-                println!("Error sending message: {:?}", why);
-            }, 
+            "ping" => {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "pong!").await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+            "remind" => {
+                if let Err(why) = msg
+                    .channel_id
+                    .say(&ctx.http, "@goomfy buy a controller")
+                    .await
+                {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+            "rps" => {
+                let x: f32 = rand::prelude::random();
+                let res = if x <= 0.33 {
+                    "You lost"
+                } else if x <= 0.66 {
+                    "You win"
+                } else {
+                    "We tied"
+                };
+
+                if let Err(why) = msg.channel_id.say(&ctx.http, res).await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
             _ => {}
         }
     }
@@ -47,14 +70,15 @@ async fn main() {
     // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     // Create a new instance of the Client, logging in as a bot. This will
     // automatically prepend your bot token with "Bot ", which is a requirement
     // by Discord for bot users.
-    let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
 
     // Finally, start a single shard, and start listening to events.
     //
